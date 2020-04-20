fc1e4ac6c42d0a9add5f21ebac3c95e80fc75689
agus tri adzie <agusmemons90@gmail.com>
2020-04-20 20:26:50 +0700

    setup

--- index.js
+++ index.js
@@ -1,111 +1,120 @@
+/**
+ * @format
+ */
+
 const multer = require('multer');
 const axios = require('axios');
 const FormData = require('form-data');
-const fs = require('fs') ;
+const fs = require('fs');
 
-const uploadConfig = require('../../config/upload');
+// eslint-disable-next-line import/no-unresolved
+const uploadConfig = require('../../../config/upload');
 
-const { fileTmp } = uploadConfig; 
+const { fileTmp } = uploadConfig;
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, fileTmp)
+    destination: (req, file, cb) => {
+        cb(null, fileTmp);
     },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-  })
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
 
-
-function fileFilter (req, file, cb){
-    
-    let msg = {
-        header : {
-            message : 'Error',
-            status : 500,
-            access : null
-        },data:null
+function fileFilter(req, file, cb) {
+    const msg = {
+        header: {
+            message: 'Error',
+            status: 500,
+            access: null,
+        },
+        data: null,
     };
 
-    if(file){ // only running with content-type multipart/form
-        let
-        splitFilename = file.originalname.split(' '),
-        joinFilename = splitFilename.join('_'),
-        split_ext = joinFilename.split('.'),
-        ext       = split_ext[1],
-        check_ext = uploadConfig.fileExt.indexOf(ext.toLowerCase()); 
-        
-        if(split_ext.length > 2){
+    if (file) {
+        // only running with content-type multipart/form
+        const splitFilename = file.originalname.split(' ');
+        const joinFilename = splitFilename.join('_');
+        const splitExt = joinFilename.split('.');
+        const ext = splitExt[1];
+        const checkExt = uploadConfig.fileExt.indexOf(ext.toLowerCase());
+
+        if (splitExt.length > 2) {
             msg.header.message = uploadConfig.message.error.filename;
             return cb(msg);
         }
-        if(check_ext < 0){ 
+        if (checkExt < 0) {
             msg.header.message = uploadConfig.message.error.supportExt;
-            return cb(msg); 
+            return cb(msg);
         }
-        if(file.size > uploadConfig.fileSize){
+        if (file.size > uploadConfig.fileSize) {
             msg.header.message = uploadConfig.message.error.size;
-            return cb(msg); 
+            return cb(msg);
         }
 
+        // eslint-disable-next-line no-param-reassign
         file.data = {
-            filename : split_ext[0]+'_'+Date.now()+'.'+ext,
-            path : uploadConfig.filePath
-        }
+            filename: `${splitExt[0]}_${Date.now()}.${ext}`,
+            path: uploadConfig.filePath,
+        };
+        // eslint-disable-next-line no-param-reassign
         file.originalname = file.data.filename;
-        
-        
-        return cb(null,true);
-    }else{
-        return cb(null,false); // request not multipart
+
+        return cb(null, true);
     }
+    // request not multipart
+    return cb(null, false);
 }
 
-
 const uploads = multer({
-                    fileFilter : fileFilter,
-                    storage : storage,
-                    limits : uploadConfig.limits
-                });
-const actionUpload = ['replace','save'];
+    fileFilter,
+    storage,
+    limits: uploadConfig.limits,
+});
+const actionUpload = ['replace', 'save'];
 
-module.exports =  {
-    files_upload : uploads,
-    multer_err  : multer.MulterError,
-	sendToFileManager : (data,callback_send)=>{
-        var bodyFormData = new FormData();
-        if (actionUpload.indexOf(data.action) !== -1){
-            bodyFormData.append('filebuffer', fs.createReadStream(`${fileTmp}/${data.filename}`));    
+module.exports = {
+    files_upload: uploads,
+    multer_err: multer.MulterError,
+    sendToFileManager: (data, cb) => {
+        const bodyFormData = new FormData();
+        if (actionUpload.indexOf(data.action) !== -1) {
+            bodyFormData.append(
+                'filebuffer',
+                fs.createReadStream(`${fileTmp}/${data.filename}`),
+            );
         }
-        
+
         bodyFormData.append('filepath', `${data.path}${data.filename}`);
         bodyFormData.append('action', data.action);
 
-        axios.create({
-            headers: bodyFormData.getHeaders()
-        }).post(uploadConfig.urlStore,bodyFormData)
-        .then(function (response) {
-            if(response.data.header.status == 200){
-                fs.unlinkSync(`${fileTmp}/${data.filename}`);
-                return callback_send(null, data);
-            }else{
-                return callback_send(true,'Upload Failed')
-            }
-        }).catch(function (error) {
-            return callback_send(true,'Upload Error')
-        });
+        axios
+            .create({
+                headers: bodyFormData.getHeaders(),
+            })
+            .post(uploadConfig.urlStore, bodyFormData)
+            .then((response) => {
+                if (response.data.header.status === 200) {
+                    fs.unlinkSync(`${fileTmp}/${data.filename}`);
+                    return cb(null, data);
+                }
+                return cb(true, 'Upload Failed');
+            })
+            .catch((error) => {
+                return cb(true, error);
+            });
     },
-    getFileManager : (data,callback_send)=>{
-        axios.get(`${uploadConfig.urlStore}?filepath=${data.path}`)
-        .then(function (response) {
-            if(response.data.header.status == 200){
-                return callback_send(null, response.data);
-            }else{
-                return callback_send(true,'Get file failed')
-            }
-        }).catch(function (error) {
-            return callback_send(true,'Cannot get file')
-        });
-    }
+    getFileManager: (data, cb) => {
+        axios
+            .get(`${uploadConfig.urlStore}?filepath=${data.path}`)
+            .then((response) => {
+                if (response.data.header.status === 200) {
+                    return cb(null, response.data);
+                }
+                return cb(true, 'Get file failed');
+            })
+            .catch((error) => {
+                return cb(true, error);
+            });
+    },
 };
-
